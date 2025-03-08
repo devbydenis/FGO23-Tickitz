1b4b9e5fc68cc82a5380a1d2dbaf49baa8d2aea1
devbydenis <hello.denisrahmadi@gmail.com>
2025-03-08 23:35:38 +0700

    add: section payment timeline

--- payment/payment.js
+++ payment/payment.js
@@ -6,9 +6,22 @@ wrapper.addEventListener("click", () => {
     const input = element.querySelector("input");
     // console.log(input);
     if (input.checked) {
-      input.parentElement.style.border = "1px solid blue";
+      input.parentElement.style.border = "2px solid blue";
       return;
     }
     input.parentElement.style.border = "none";
+    input.parentElement.style.border = "2px solid var(--lightgrey)";
   });
 });
+
+
+const payYourOrder = document.querySelector(".pay-order-btn")
+const inputPersonalInfo = document.querySelector(".personal-info div input")
+const listPaymentMethod= document.querySelector(".payment-method ul li")
+const main = document.querySelector(".payment-main")
+payYourOrder.addEventListener('click', () => {
+  inputPersonalInfo.style.zIndex = '-1'
+  inputPersonalInfo.style.position = 'relative'
+  listPaymentMethod.style.zIndex = '-1'
+
+})
